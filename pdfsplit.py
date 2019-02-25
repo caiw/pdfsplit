0bd5abcfde2c76984d74a6aed54a609c0a0dacea
Cai <cai@caiwingfield.com>
2019-02-25 11:31:32 +0000

    Extra logging

--- pdfsplit.py
+++ pdfsplit.py
@@ -12,15 +12,16 @@ stdout = nonbuffered_stdout
        program_description="Split a PDF on its blank pages. Segments will be saved in the same directory as the "
                            "original file.")
 def main():
+
+	text_preview_length = 30
+
     parser = GooeyParser()
     parser.add_argument("path",
                         type=str,
                         widget='FileChooser',
                         # title="Combined PDF file."
                         )
-
     args = parser.parse_args()
-
     combined_path = args.path
 
     print(f"Loading \"{path.basename(combined_path)}\"...")
@@ -36,12 +37,15 @@ def main():
 
         for page_i in range(total_pages):
             p = combined_pdf.getPage(page_i)
-            if len(p.extractText()) == 0:
-                # blank page
+            page_text = p.extractText()
+            if len(page_text) == 0:
+                print(f"Page {page_i} was blank, splitting here...")
                 empty_buffer(this_file_page_buffer, outfile_i, combined_path)
                 this_file_page_buffer = []
                 outfile_i += 1
             else:
+            	print(f"Page {page_i} contained text \"{page_text[:text_preview_length]}...\"")
+            	print(f"Adding to next document buffer")
                 this_file_page_buffer.append(p)
 
         empty_buffer(this_file_page_buffer, outfile_i, combined_path)
@@ -49,10 +53,11 @@ def main():
 
 def empty_buffer(this_file_page_buffer, outfile_i, combined_path):
     out_filename = path.join(path.dirname(combined_path),
-                             f"{path.splitext(path.basename(combined_path))[0]} split {outfile_i:02}.pdf")
+                             f"{path.splitext(path.basename(combined_path))[0]} - split {outfile_i:02}.pdf")
     if path.isfile(out_filename):
         raise FileExistsError(f"Tried to save file {path.basename(out_filename)} but it already exists. Please delete "
                               f"or move so it doesn't get overwrittn.")
+	print(f"Saving current page buffer to \"{path.basename(out_filename)}\"...")
     out_pdf = PdfFileWriter()
     for buffered_page in this_file_page_buffer:
         out_pdf.addPage(buffered_page)
